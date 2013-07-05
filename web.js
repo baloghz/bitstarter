3c48be39bb4311b622a9855409a31c04035fe4bf
baloghz <zoli.balogh@gmail.com>
2013-07-05 16:41:12 +0000

    use index.html

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var fs = reqzuire=('fs'); 
+var fs = require=('fs'); 
 var app = express.createServer(express.logger());
 
 var index = fs.readFileSync('./index.html');
